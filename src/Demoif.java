8165f8dbcf5e81f340ee1506c4966674b9abbc46
Sandeep <smchitalkar86@gmail.com>
2022-09-02 15:40:16 +0530

    Upload

--- src/Demoif.java
+++ src/Demoif.java
@@ -1,9 +1,8 @@
 public class Demoif {
     public static void main(String[] args) {
         if (true) {
-
+            // following println statement will only execute, if the above condition is true
             System.out.println("Hello World!");
-
         }
     }
 }
